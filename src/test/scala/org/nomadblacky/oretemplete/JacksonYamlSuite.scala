b5b1ad0c874b03023f48d85e3ac9aa4f0fca092c
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-05-01 23:37:07 +0900

    [wip] jackson with scala

--- src/test/scala/org/nomadblacky/oretemplete/JacksonYamlSuite.scala
+++ src/test/scala/org/nomadblacky/oretemplete/JacksonYamlSuite.scala
@@ -3,6 +3,7 @@ package org.nomadblacky.oretemplete
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import org.scalatest.FunSuite
 
 /**
@@ -13,13 +14,22 @@ class JacksonYamlSuite extends FunSuite {
   case class Hoge(id: Int, name: String)
 
   test("jackson-yaml sample") {
-    val mapper = new ObjectMapper(new YAMLFactory())
+    val mapper = new ObjectMapper(new YAMLFactory()) with ScalaObjectMapper
     mapper.registerModule(DefaultScalaModule)
     val yaml =
-      """- id: 1
-        |  name: hoge
+      """---
+        |id: 1
+        |name: "foo"
       """.stripMargin
-    val myCaseClass = mapper.readValue(yaml, Hoge.getClass)
-    println(myCaseClass)
+    val hoge = mapper.readValue[Hoge](yaml)
+    println(hoge)
+  }
+
+  test("hoge") {
+    val hoge = Hoge(1, "foo")
+    val mapper = new ObjectMapper(new YAMLFactory()) with ScalaObjectMapper
+    mapper.registerModule(DefaultScalaModule)
+    val yaml = mapper.writeValueAsString(hoge)
+    println(yaml)
   }
 }
